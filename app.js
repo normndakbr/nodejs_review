6f7ef6ac54b9d68e8d1474ccf1afd052ca4a769c
Normandia Akbar <normnd.akbr2@gmail.com>
2024-05-09 16:16:01 +0800

    fetch data from data.json using get

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
-app.get('/',(req, res) => {
-    res.send('Hello World from Express.js!');
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World from Express.js!</h1><br>This is Homepage');
+});
+
+app.get('/pokemons', (req, res) => {
+    fs.readFile('./data.json', 'utf-8', (err, data) => {
+        if (err) {
+            res.send(err.message);
+        } else {
+            res.send(data);
+        }
+    });
 });
 
 app.listen(port, () => {
