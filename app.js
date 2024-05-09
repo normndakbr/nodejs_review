58f93fbf2a0cd780a3dcb6518dbb8c9f79e57f74
Normandia Akbar <normnd.akbr2@gmail.com>
2024-05-10 07:33:24 +0800

    added nodemon package

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
 
 app.listen(PORT, () => {
     console.log("Application is running on http://localhost:" + PORT);
