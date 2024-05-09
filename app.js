bc1a06caea96d334f22a9cd5c71bdebed18187f4
Normandia Akbar <normnd.akbr2@gmail.com>
2024-05-09 16:39:30 +0800

    added routes with params

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World from Express.js!</h1><br>This is Homepage');
 });
 
-app.get('/pokemons', (req, res) => {
+app.get('/pokemon', (req, res) => {
     fs.readFile('./data.json', 'utf-8', (err, data) => {
         if (err) {
             res.send(err.message);
@@ -15,6 +15,11 @@ app.get('/pokemons', (req, res) => {
             res.send(data);
         }
     });
+});
+
+// get data with get params /:id
+app.get('/pokemon/:id', (req, res) => {
+    console.log("req params = ", req.params);
 });
 
 app.listen(port, () => {
