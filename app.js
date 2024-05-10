f2cc001a9320533f25b23825766b143393148402
Normandia Akbar <normnd.akbr2@gmail.com>
2024-05-10 14:22:28 +0800

    get data from departements.json and create instance departement

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const fs = require('fs');
+const FactoryMasterData = require('./masterData');
 
 app.set("view engine", "ejs");
 
 app.get('/about', (req, res) => {
     res.render("home");
+});
+
+app.get("/departements", (req, res) => {
+    // baca file
+    fs.readFile("./departements.json", "utf-8", (err, data) => {
+        if(err){
+            res.send(err);
+        } else {
+            data = JSON.parse(data);
+            const instanceDepartement = data.map( departement => {
+                return FactoryMasterData.create(departement);
+            })
+
+            console.log(instanceDepartement);
+            // buat menjadi instance
+            // array of instance
+            // render ke products.ejs {}
+            // res.render("products");
+        }
+    });
+    // buat menjadi instance, array of instance, render ke departements.ejs
+    res.render("departements");
 });
 
 app.listen(PORT, () => {
