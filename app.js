f7b1c227fa42b2e5e76abe7365db5a82ac779c72
Normandia Akbar <normnd.akbr2@gmail.com>
2024-05-10 17:51:58 +0800

    added page for add Master Data

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const PORT = 3000;
 const fs = require('fs');
 const FactoryMasterData = require('./masterData');
 
+// add app.use... to use req.body
+app.use(express.urlencoded({ extended: false }));
+
+// apply ejs as current view engine
 app.set("view engine", "ejs");
 
 app.get('/about', (req, res) => {
     res.render("home");
 });
 
-app.get("/departements", (req, res) => {
+app.get("/masterData", (req, res) => {
     console.log("test");
     // baca file
     fs.readFile("./departements.json", "utf-8", (err, data) => {
@@ -28,6 +32,14 @@ app.get("/departements", (req, res) => {
             res.render("departements", { instanceDepartement });
         }
     });
+});
+
+app.get("/add-masterData", (req, res) => {
+    res.render("addMasterData");
+});
+
+app.post("/add-masterData", (req, res) => {
+    
 });
 
 app.listen(PORT, () => {
