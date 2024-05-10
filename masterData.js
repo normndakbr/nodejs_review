1473d2282992073018cde6de1153603d3ba82c6b
Normandia Akbar <normnd.akbr2@gmail.com>
2024-05-10 08:47:23 +0800

    added master data

--- masterData.js
+++ masterData.js
@@ -1,7 +1,7 @@
-class MasterData{
-    constructor(id, idParent, code, name, type, status, createdAt, updatedAt){
+class MasterData {
+    constructor(id, parentId, code, name, type, status, createdAt, updatedAt) {
         this.id = id;
-        this.idParent = idParent;
+        this.parentId = parentId;
         this.code = code;
         this.name = name;
         this.status = status;
@@ -12,25 +12,35 @@ class MasterData{
 }
 
 class Departement extends MasterData {
-    constructor(id, code, name, status, createdAt, updatedAt){
-        super(id, code, name, "departement", status, createdAt, updatedAt);
+    constructor(id, code, name, status, createdAt, updatedAt) {
+        super(id, 0, code, name, "departement", status, createdAt, updatedAt);
     }
 }
 
 class Section extends MasterData {
-    constructor(id, code, name, status, createdAt, updatedAt){
-        super(id, code, name, "section", status, createdAt, updatedAt);
+    constructor(id, parentId, code, name, status, createdAt, updatedAt) {
+        super(id, parentId, code, name, "section", status, createdAt, updatedAt);
     }
 }
 
 class Position extends MasterData {
-    constructor(id, code, name, status, createdAt, updatedAt){
-        super(id, code, name, "position", status, createdAt, updatedAt);
+    constructor(id, parentId, code, name, status, createdAt, updatedAt) {
+        super(id, parentId, code, name, "position", status, createdAt, updatedAt);
     }
 }
 
-class FactoryMasterData{
-    static create(obj){
-
+class FactoryMasterData {
+    static create(obj) {
+        if (obj.type === "departement") {
+            return new Departement(obj.id, obj.code, obj.name, obj.status, obj.createdAt, obj.updatedAt);
+        } else if (obj.type === "section") {
+            return new Section(obj.id, obj.parentId, obj.code, obj.name, obj.status, obj.createdAt, obj.updatedAt);
+        } else if (obj.type === "position") {
+            return new Position(obj.id, obj.parentId, obj.code, obj.name, obj.status, obj.createdAt, obj.updatedAt);
+        } else {
+            return new MasterData(obj.id, obj.parentId, obj.code, obj.name, obj.type, obj.status, obj.createdAt, obj.updatedAt);
+        }
     }
 }
+
+module.exports = FactoryMasterData;
