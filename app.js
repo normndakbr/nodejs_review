c19c7aa3bf5cfc7a1cf34eadd6efce690034aafb
Normandia Akbar <normnd.akbr2@gmail.com>
2024-05-10 08:53:53 +0800

    added home.ejs and use res.render on app.js

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
+app.set("view engine", "ejs");
+
+app.get('/about', (req, res) => {
+    res.render("home");
 });
 
 app.listen(PORT, () => {
